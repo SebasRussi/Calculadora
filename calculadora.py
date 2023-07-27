bdf6c19e640cb69cb38b600dabbf6a70a4000f16
sebas <srr1767@gmail.com>
2023-07-27 18:40:04 -0500

    Add multiplicar y dividir function

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,11 @@
 def sumar(numero1, numero2):
     return numero1 + numero2
+
+
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+
+
+def dividir(numero1, numero2):
+    if numero2 != 0:
+        return numero1 / numero2
